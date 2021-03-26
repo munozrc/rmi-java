183f7eb611ad6909118beac4eebf9f9aefe6e362
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-03-25 21:31:44 -0500

    add class Server

--- src/main/java/main/Server.java
+++ src/main/java/main/Server.java
@@ -1,10 +1,40 @@
 package main;
 
+import java.rmi.AlreadyBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
 public class Server {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws RemoteException, AlreadyBoundException {
 
-    System.out.println("Hello World from Server");
+    Remote stub = UnicastRemoteObject.exportObject(new Calculadora() {
+      @Override
+      public float sumar(float a, float b) throws RemoteException {
+        return a + b;
+      }
+
+      @Override
+      public float restar(float a, float b) throws RemoteException {
+        return a + b;
+      }
+
+      @Override
+      public float multiplicar(float a, float b) throws RemoteException {
+        return a * b;
+      }
+
+      @Override
+      public float dividir(float a, float b) throws RemoteException {
+        return a / b;
+      }
+    }, 0);
+
+    Registry registry = LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
+    registry.bind("Calculadora", stub);
 
   }
 }
